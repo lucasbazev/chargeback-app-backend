d45da07c9d403b7354e9855fdf7af7942ad551c9
Lucas Azevedo <lucasbazev@gmail.com>
2026-02-08 14:40:30 -0300

    chore: postgres db configuration

--- Package.swift
+++ Package.swift
@@ -2,37 +2,44 @@
 import PackageDescription
 
 let package = Package(
-    name: "backend",
-    platforms: [
-       .macOS(.v13)
-    ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
-        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "backend",
-            dependencies: [
-                .product(name: "Vapor", package: "vapor"),
-                .product(name: "NIOCore", package: "swift-nio"),
-                .product(name: "NIOPosix", package: "swift-nio"),
-            ],
-            swiftSettings: swiftSettings
-        ),
-        .testTarget(
-            name: "backendTests",
-            dependencies: [
-                .target(name: "backend"),
-                .product(name: "VaporTesting", package: "vapor"),
-            ],
-            swiftSettings: swiftSettings
-        )
-    ]
+  name: "backend",
+  platforms: [
+    .macOS(.v13)
+  ],
+  dependencies: [
+    // 💧 A server-side Swift web framework.
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+    // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+    .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+    // DB
+    .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+    .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
+  ],
+  targets: [
+    .executableTarget(
+      name: "backend",
+      dependencies: [
+        .product(name: "Fluent", package: "fluent"),
+        .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+        .product(name: "Vapor", package: "vapor"),
+        .product(name: "NIOCore", package: "swift-nio"),
+        .product(name: "NIOPosix", package: "swift-nio"),
+      ],
+      swiftSettings: swiftSettings
+    ),
+    .testTarget(
+      name: "backendTests",
+      dependencies: [
+        .target(name: "backend"),
+        .product(name: "VaporTesting", package: "vapor"),
+      ],
+      swiftSettings: swiftSettings
+    ),
+  ]
 )
 
-var swiftSettings: [SwiftSetting] { [
-    .enableUpcomingFeature("ExistentialAny"),
-] }
+var swiftSettings: [SwiftSetting] {
+  [
+    .enableUpcomingFeature("ExistentialAny")
+  ]
+}
--- Sources/backend/configure.swift
+++ Sources/backend/configure.swift
@@ -1,10 +1,26 @@
+import Fluent
+import FluentPostgresDriver
 import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  // POSTGRES DB
+  app.databases.use(
+    .postgres(
+      configuration: .init(
+        hostname: "localhost",
+        username: "chargeback_admin",
+        password: "chargeback-app-db-admin",
+        database: "chargeback_app_db",
+        tls: .disable
+      )
+    ),
+    as: .psql
+  )
 
-    // register routes
-    try routes(app)
+  // uncomment to serve files from /Public folder
+  // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+  // register routes
+  try routes(app)
 }
